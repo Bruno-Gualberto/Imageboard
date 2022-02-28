70e878b04a5cee4052cc2f4850ac145195bf8557
Bruno Gualberto <brunofgualberto@gmail.com>
2022-02-28 16:33:52 +0100

    setting up imageboard

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 
 app.use(express.static('./public'));
 
+// middleware to properly access incoming requests of content-type application/json
 app.use(express.json());
 
+
+// this route should come at last
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
